e7147c727504147aa91f3b5b100453b44d62bc82
tison <wander4096@gmail.com>
2024-03-10 23:22:31 +0800

    more
    Signed-off-by: tison <wander4096@gmail.com>

--- keyboard-monitor/display.py
+++ keyboard-monitor/display.py
@@ -14,7 +14,8 @@ conn = st.connection(
 
 df = conn.query("SELECT COUNT(*) AS total_hits FROM keyboard_monitor")
 st.metric("Total hits", df.total_hits[0])
-most_frequent_key, most_frequent_combos = st.columns(2)
+
+most_frequent_key, most_frequent_combo = st.columns(2)
 df = conn.query("""
 SELECT hits, COUNT(*) as times
 FROM keyboard_monitor
@@ -30,4 +31,26 @@ WHERE hits LIKE '%+%'
 GROUP BY hits
 ORDER BY times DESC limit 1;
 """)
-most_frequent_combos.metric("Most frequent combos", df.hits[0])
+most_frequent_combo.metric("Most frequent combo", df.hits[0])
+
+top_frequent_keys, top_frequent_combos = st.columns(2)
+df = conn.query("""
+SELECT hits, COUNT(*) as times
+FROM keyboard_monitor
+WHERE hits NOT LIKE '%+%'
+GROUP BY hits
+ORDER BY times DESC limit 10;
+""")
+top_frequent_keys.header("Top 10 keys")
+top_frequent_keys.dataframe(df)
+df = conn.query("""
+SELECT hits, COUNT(*) as times
+FROM keyboard_monitor
+WHERE hits LIKE '%+%'
+GROUP BY hits
+ORDER BY times DESC limit 10;
+""")
+top_frequent_combos.header("Top 10 combos")
+top_frequent_combos.dataframe(df)
+
+# 一个日期选择器展示当天每个小时的 hits 数量
