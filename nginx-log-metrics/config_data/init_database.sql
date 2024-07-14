d6df4f71e526bde83913479cd689dde28dfa4c99
Ruihang Xia <waynestxia@gmail.com>
2024-07-14 17:42:30 +0800

    feat: create flow
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ nginx-log-metrics/config_data/init_database.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `ngx_access_log` (
+  `client` STRING NULL,
+  `referer` STRING NULL,
+  `request` STRING NULL,
+  `status` SMALLINT UNSIGNED NULL,
+  `size` INT UNSIGNED NULL,
+  `agent` STRING NULL,
+  `access_time` TIMESTAMP(3) NOT NULL,
+  TIME INDEX (`access_time`)
+)
+WITH(
+  append_mode = 'true'
+);
+
+-- min(size) as min_size, max(size) as max_size, avg(size) as avg_size
+CREATE FLOW ngx_aggregation
+SINK TO ngx_statistics
+AS 
+SELECT 
+    status,
+    count(client) AS total_logs
+FROM ngx_access_log 
+GROUP BY
+    status,
+    tumble(access_time, '1 minutes', '2024-01-01 00:00:00');
+
+SHOW TABLES;
