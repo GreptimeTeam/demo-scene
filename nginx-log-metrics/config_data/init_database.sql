2f580628c08c276f0f0aa12b619a647362f4d322
Ruihang Xia <waynestxia@gmail.com>
2024-07-16 23:02:04 +0800

    feat: add complex flow examples
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- nginx-log-metrics/config_data/init_database.sql
+++ nginx-log-metrics/config_data/init_database.sql
@@ -6,7 +6,7 @@ CREATE TABLE `ngx_access_log` (
   `endpoint` STRING NULL,
   `protocol` STRING NULL,
   `status` SMALLINT UNSIGNED NULL,
-  `size` INT UNSIGNED NULL,
+  `size` DOUBLE NULL,
   `agent` STRING NULL,
   `access_time` TIMESTAMP(3) NOT NULL,
   TIME INDEX (`access_time`)
@@ -15,16 +15,32 @@ WITH(
   append_mode = 'true'
 );
 
--- min(size) as min_size, max(size) as max_size, avg(size) as avg_size
+CREATE TABLE `ngx_statistics` (
+  `status` SMALLINT UNSIGNED NULL,
+  `total_logs` BIGINT NULL,
+  `min_size` DOUBLE NULL,
+  `max_size` DOUBLE NULL,
+  `avg_size` DOUBLE NULL,
+  `high_size_count` DOUBLE NULL,
+  `time_window` TIMESTAMP time index,
+  `update_at` TIMESTAMP NULL,
+  PRIMARY KEY (`status`)
+);
+
 CREATE FLOW ngx_aggregation
 SINK TO ngx_statistics
 AS 
 SELECT 
     status,
-    count(client) AS total_logs
+    count(client) AS total_logs,
+    min(size) as min_size,
+    max(size) as max_size,
+    avg(size) as avg_size,
+    sum(case when `size` > 100::double then 1::double else 0::double end) as high_size_count,
+    date_bin(INTERVAL '1 minutes', access_time, '2024-01-01 00:00:00'::TimestampNanosecond) as time_window,
 FROM ngx_access_log 
 GROUP BY
     status,
-    tumble(access_time, '1 minutes', '2024-01-01 00:00:00');
+    time_window;
 
 SHOW TABLES;
