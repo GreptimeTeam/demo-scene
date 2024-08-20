3d364d873b1c3b921b8c087da51ead6cb4b8b594
Ning Sun <sunng@protonmail.com>
2024-08-20 16:07:01 +0800

    feat: corrected producer and vector

--- kafka-log/producer/app.py
+++ kafka-log/producer/app.py
@@ -2,9 +2,15 @@ from kafka import KafkaProducer
 import time
 import os
 
+print("starting producer", flush=True)
+
 topic = os.environ["KAFKA_TOPIC_NAME"]
+print("using topic: ", topic, flush=True)
 
 producer = KafkaProducer(bootstrap_servers="kafka:9092")
+print("using producer: ", producer, flush=True)
+
 while True:
-    producer.send(topic, "this is a test message")
-    time.sleep(3000)
+    producer.send(topic, b"this is a test message")
+    print("message sent", flush=True)
+    time.sleep(3)
