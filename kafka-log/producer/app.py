87849ef836fefa561ae57dd3ecee51db35f1a077
Ning Sun <sunng@protonmail.com>
2024-08-20 12:23:02 +0800

    feat: initial kafka topic

--- kafka-log/producer/app.py
+++ kafka-log/producer/app.py
@@ -1,7 +1,10 @@
 from kafka import KafkaProducer
 import time
+import os
+
+topic = os.environ["KAFKA_TOPIC_NAME"]
 
 producer = KafkaProducer(bootstrap_servers="kafka:9092")
 while True:
-    producer.send("test-topic", "this is a test message")
+    producer.send(topic, "this is a test message")
     time.sleep(3000)
