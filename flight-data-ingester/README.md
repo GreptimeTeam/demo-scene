6e02d38f26f0462ddda8f2a27d39b3bf728830cf
truedrju <76677784+truedrju@users.noreply.github.com>
2024-08-30 09:09:14 -0700

    adds demo scripts and docker compose files

---
+++ flight-data-ingester/README.md
@@ -0,0 +1,38 @@
+# GreptimeDB Flight
+
+This project demonstrates GreptimeDB's ingesting geo-spatial data using the the `greptimedb-ingester-go` client. It selects the last 10 flights that departed in the last 30 minutes from the configured icao airport code. The ingester script utilizes the [OpenSky Network API](https://opensky-network.org/apidoc/) to fetch flight state data and inserts the flight metrics into GreptimeDB
+
+## How to run this demo
+
+Ensure you have `git`, `docker`, `docker-compose`
+installed. To run this demo:
+
+```shell
+git clone https://github.com/GreptimeTeam/demo-scene.git
+cd demo-scene/flight-data-ingester
+docker compose up
+```
+
+It can take a while for the first run to pull down images and also build necessary components.
+
+## How it works
+
+The topology is illustrated in this diagram.
+
+```mermaid
+flowchart LR
+  greptimedb[(GreptimeDB)]
+
+  api{Open Sky Data} --> go-ingester
+  go-ingester --> greptimedb
+```
+
+after GreptimeDB starts, we use the `ingester` script which uses the go client's [high level api](https://docs.greptime.com/user-guide/ingest-data/for-iot/grpc-sdks/go/#installation) to create the table and insert data. It's dead-simple to perform transformations and data munging on your struct and insert into target GreptimeDB columns by tagging your metric struct accordingly as seen in the `./ingester/dto.go` file .
+
+## Note
+
+please update the ICAO flight code for your local airport of the city you are running the demo in to ensure there is data in the set. This can be done by updating the ICAO_AIRPORT_CODE environment variable in your docker compose file
+
+If you are going to restart this demo, press `Ctrl-C` and remember to call
+`docker compose down` to clean up the data before you run `docker compose up`
+again.
---
+++ flight-data-ingester/ingester/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"strings"
+
+	greptime "github.com/GreptimeTeam/greptimedb-ingester-go"
+
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"syscall"
+	"time"
+)
+
+var (
+	ICAO_AIRPORT_CODE string
+	numFlights        = 10
+)
+
+var greptimeClient *greptime.Client
+
+func init() {
+	ICAO_AIRPORT_CODE = os.Getenv("ICAO_AIRPORT_CODE")
+	if ICAO_AIRPORT_CODE == "" {
+		log.Fatalf("❌ ICAO_AIRPORT_CODE environment variable is not set")
+	}
+
+	cfg := greptime.NewConfig("greptimedb").
+		WithDatabase("public").
+		WithAuth(os.Getenv("GREPTIME_USERNAME"), os.Getenv("GREPTIME_PASSWORD"))
+
+	var err error
+	greptimeClient, err = greptime.NewClient(cfg)
+	if err != nil {
+		log.Fatalf("❌ Failed to create GreptimeDB client: %v", err)
+	}
+
+	fmt.Println("📌 GreptimeDB client initialized successfully")
+}
+
+func main() {
+
+	fmt.Println("📌 starting flight-data-ingester demo")
+
+	trackFlights, err := SelectLiveFlights(ICAO_AIRPORT_CODE, numFlights)
+	if err != nil {
+		log.Fatalf("❌ could not select live flights %+v\n", err)
+	}
+
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-ticker.C:
+			statesResponse, err := GetFlightState(trackFlights)
+			if err != nil {
+				fmt.Printf("❌ could not get flight state: %+v\n", err)
+				continue
+			}
+
+			flightMetrics := statesResponse.ToFlightMetrics()
+
+			resp, err := greptimeClient.WriteObject(context.Background(), flightMetrics)
+			if err != nil {
+				fmt.Printf("❌ could not write to greptime: %+v\n", err)
+				continue
+			}
+			log.Printf("inserted rows: %d\n", resp.GetAffectedRows().GetValue())
+
+		case <-done:
+			fmt.Println("Received termination signal. Shutting down...")
+			return
+		}
+	}
+}
+
+func GetFlightState(flight []Flight) (*StateResponse, error) {
+	icaoCsv := ""
+	icaos := make([]string, len(flight))
+	for _, f := range flight {
+		icaos = append(icaos, f.Icao24)
+	}
+	icaoCsv = strings.Join(icaos, ",")
+	url := fmt.Sprintf("https://opensky-network.org/api/states/all?icao24=%s", icaoCsv)
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("unexpected status code: %d, and error reading response body: %v", resp.StatusCode, err)
+		}
+		bodyString := string(bodyBytes)
+		return nil, fmt.Errorf("unexpected status code: %d : %s", resp.StatusCode, bodyString)
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	var statesResponse StateResponse
+	err = json.Unmarshal(bodyBytes, &statesResponse)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding response: %v - response body: %s", err, string(bodyBytes))
+	}
+
+	if len(statesResponse.States) == 0 {
+		return nil, fmt.Errorf("no aircraft data found for the given flights")
+	}
+
+	fmt.Printf("selected: state data for %+v flights\n", len(statesResponse.States))
+	return &statesResponse, nil
+}
+
+func SelectLiveFlights(airportCode string, num int) ([]Flight, error) {
+	endTime := time.Now().Unix()
+	startTime := endTime - 60*30 // all flights leaving in last 30 minutes
+
+	url := fmt.Sprintf("https://opensky-network.org/api/flights/departure?begin=%d&end=%d&airport=%s", startTime, endTime, airportCode)
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("unexpected status code: %d, and error reading response body: %v", resp.StatusCode, err)
+		}
+		bodyString := string(bodyBytes)
+		return nil, fmt.Errorf("unexpected status code: %d : %s", resp.StatusCode, bodyString)
+	}
+
+	var flights []Flight
+
+	if err := json.NewDecoder(resp.Body).Decode(&flights); err != nil {
+		return nil, fmt.Errorf("error decoding response: %+v", err)
+	}
+
+	if len(flights) == 0 {
+		return nil, fmt.Errorf("no flights found in the last 10 minutes")
+	}
+
+	if num > len(flights) {
+		num = len(flights)
+	}
+
+	return flights[:num], nil
+
+}
