67d93dcb2164bbfc3688c118101e19e8376cbe65
Ning Sun <classicning@gmail.com>
2024-09-07 11:40:32 -0700

    feat: add another example to ingest metrics from kafka (#44)

--- kafka-ingestion/producer/app.py
+++ kafka-ingestion/producer/app.py
@@ -4,13 +4,22 @@ import os
 
 print("starting producer", flush=True)
 
-topic = os.environ["KAFKA_TOPIC_NAME"]
-print("using topic: ", topic, flush=True)
+log_topic = os.environ["KAFKA_LOG_TOPIC_NAME"]
+print("using topic: ", log_topic, flush=True)
+metric_topic = os.environ["KAFKA_METRIC_TOPIC_NAME"]
+print("using topic: ", metric_topic, flush=True)
 
 producer = KafkaProducer(bootstrap_servers="kafka:9092")
 print("using producer: ", producer, flush=True)
 
 while True:
-    producer.send(topic, b"this is a test message")
+    producer.send(
+        log_topic,
+        b'127.0.0.1 - - [04/Sep/2024:15:46:13 -0700] "GET / HTTP/1.1" 200 615 "-" "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"',
+    )
+    producer.send(
+        metric_topic,
+        b"monitor,host=host1 cpu=1.2",
+    )
     print("message sent", flush=True)
     time.sleep(3)
