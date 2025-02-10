8f20e314b2ce1125f37d96781c2a8ec8ec9a0a8f
Dennis Zhuang <killme2008@gmail.com>
2025-02-10 17:31:26 +0800

    chore: use langchain-ollama

--- ollama-opentelemetry/joke.py
+++ ollama-opentelemetry/joke.py
@@ -1,8 +1,9 @@
 import openlit
-from langchain_community.llms import Ollama
+
+from langchain_ollama.llms import OllamaLLM
 
 openlit.init(otlp_endpoint="http://127.0.0.1:4318", disable_batch=True)
 
-llm = Ollama(model='deepseek-r1:1.5b')
+llm = OllamaLLM(model='deepseek-r1:1.5b')
 
 print(llm.invoke("Tell me a joke"))
